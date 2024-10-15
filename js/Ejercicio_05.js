e964cf22abdb093338adf714d6d3cae9940d9f88
Jonathan <230410@utxicotepec.edu.mx>
2024-10-14 21:55:27 -0600

    Practica 7
    Ciclos while, iterativo for, Breal/Continua

--- js/Ejercicio_05.js
+++ js/Ejercicio_05.js
@@ -39,3 +39,114 @@ else
 
 
 
+
+
+    console.log("%c2.- Operador Ternario ( validacion?cumple:no_cumple) ", style_console);
+    // En Java Script exista una operación simplicada que valida si una condición se cumple o no, y que hacer en cada caso
+    
+    const edadPersona = 18;
+    const mayoriaEdadMX = 18;
+    const mayoriaEdadUS = 21;
+    
+    let evaluarMayoriaEdad = (edad) =>
+        edad>=18 ? "Eres mayor de edad.": "No eres mayor de edad."
+    
+    console.log("Evaluando la mayoría de edad de una persona...")
+    console.log(evaluarMayoriaEdad(edadPersona));
+    
+    // Sin embargo tenemos que considerar que la mayoría de edad varia en cada país por cuestiones legales, por lo que debemos considerar un segundo parámmetro en la evalación.
+    
+    evaluarMayoriaEdad = (edad,pais) =>
+        (edad>=18 && pais==="MX")?`En ${pais} eres mayor de edad `:`En ${pais} NO eres mayor de edad `;
+    
+    console.log("Evaluando la mayoría de edad de una persona en México...")
+    console.log(evaluarMayoriaEdad(edadPersona, "MX"));
+    
+    console.log("Evaluando la mayoría de edad de una persona en Estados Unidos...")
+    console.log(evaluarMayoriaEdad(edadPersona, "US"));
+    
+    
+    console.log("%c3.- SwITCH - CASE ( Elección por valor definido) ", style_console);
+    
+    // Calculando tu generación en base a tu edad
+    
+    let asingaGeneracion = (anioNacimiento)=>{
+        switch(true)
+        {
+            case(anioNacimiento<1968):   //Baby Boomers 
+                return "Baby Boomers";
+    
+            case (anioNacimiento>1968 && anioNacimiento<=1980): 
+                return "Generación X";
+    
+            case (anioNacimiento>1980 && anioNacimiento<=1994):
+                return "Milenials";
+            
+            case (anioNacimiento>1994 && anioNacimiento<=2010):
+                return "Centenials";
+                
+            case (anioNacimiento>2010):
+                return "Krystal";
+        }
+    }
+    
+    console.log(`Dado que nació en el año 2005 soy de la generación: ${asingaGeneracion(2005)}`);
+
+    console.log("%c4.- Manejo de Excepciones (TRY / CATCH)", style_console);
+
+    try{//intenta
+        let result = 0/10;
+        console.log(`Intentamos dividir 0/10, el resultado es: ${result}`);
+        }catch(error){
+            console.log("Ocurrió un error: "+error.message);
+        }
+    try{//intenta
+        let result = 10/0;
+         console.log(`Intentamos dividir 10/0, el resultado es: ${result}`);
+        }catch(error){
+        console.log("Ocurrió un error: "+error.message);
+        }
+        try{//intenta
+        let a="hola";
+        let result = a/10;
+         console.log(`Intentamos dividir a/10, el resultado es: ${result}`);
+        }catch(error){
+        console.log("Ocurrió un error: "+error.message);
+         }
+        try{//intenta
+        let result = a/10;
+         console.log(result);
+        }catch(error){
+        console.log("Ocurrió un error: "+error.message);
+        }
+         try{//intenta
+        let x=8, y=2, result=x/y; //Dividir una variable no definoda entre 0
+        console.log(result);
+        }catch(error){
+        console.log("Ocurrió un error: "+error.message);
+       }
+       
+       console.log("%c5.- Control de Ciclos (BREAK / CONTINUA)", style_console);
+
+       // En algunas ocaciones será importante detener un ciclo de manera abrupta para controlar casos especiales en un ciclo...
+
+       console.log("Vamos a contar del 1 al 10....")
+       for(let num=1; num<=10; num++)
+          console.log(num);
+
+       console.log("Ahora necesitamos que si llegas al 7 pares de contar... suponiendo que solo por hoy es de mala suerte...")
+       for( num=1;num<=10;num++)
+        if(num==7)
+            break;
+        else
+        console.log(num);
+
+        
+       console.log("Ahora necesitamos que si llegas al 7 te saltes ese número y continues")
+       for( num=1;num<=10;num++){
+        if(num==7)
+            continue;
+        console.log(num);
+        }
+
+
